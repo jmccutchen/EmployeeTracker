b0f4a7e523da2213a762eeef9fc1fc933504998a
John McCutchen <purplegrits@yahoo.com>
2019-12-18 21:06:30 -0500

    finished schema

--- employeeTrackerSchema.sql
+++ employeeTrackerSchema.sql
@@ -1,18 +1,13 @@
-
 DROP DATABASE IF EXISTS employeeTracker_DB;
 CREATE DATABASE employeeTracker_DB;
 
 USE employeeTracker_DB;
 
-CREATE TABLE employee(
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT NOT NULL,
-    manager_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES role (role_id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+CREATE TABLE department(
+    department_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL UNIQUE,
+    PRIMARY KEY (department_id)
+    
 );
 
 CREATE TABLE role (
@@ -21,13 +16,20 @@ CREATE TABLE role (
     salary DECIMAL,
     department_id INT NOT NULL, 
     PRIMARY KEY (role_id),
-    FOREIGN KEY (department_id) REFERENCES department(department_id)
+    CONSTRAINT fk_dept_id FOREIGN KEY (department_id) REFERENCES department(department_id)
    
 );
 
-CREATE TABLE department(
-    department_id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30),
-    PRIMARY KEY (department_id)
-    
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_role_id FOREIGN KEY (role_id) REFERENCES role (role_id),
+    CONSTRAINT fk_manager_id FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
+
+
+
