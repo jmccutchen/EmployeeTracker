8c118b3a2b261f4c68bb97a808e1bcc05030a58b
John McCutchen <purplegrits@yahoo.com>
2019-12-20 14:44:18 -0500

    finished up 3 queries

--- employeeTrackerSchema.sql
+++ employeeTrackerSchema.sql
@@ -16,7 +16,7 @@ CREATE TABLE role (
     salary DECIMAL,
     department_id INT NOT NULL, 
     PRIMARY KEY (role_id),
-    CONSTRAINT fk_dept_id FOREIGN KEY (department_id) REFERENCES department(department_id)
+    FOREIGN KEY (department_id) REFERENCES department(department_id)
    
 );
 
@@ -25,11 +25,58 @@ CREATE TABLE employee(
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT NOT NULL,
-    manager_id INT,
+    manager_id INT DEFAULT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT fk_role_id FOREIGN KEY (role_id) REFERENCES role (role_id),
-    CONSTRAINT fk_manager_id FOREIGN KEY (manager_id) REFERENCES employee(id)
+    FOREIGN KEY (role_id) REFERENCES role (role_id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
 
+INSERT INTO department (name) VALUES
+("Sales"),
+("Engineering"),
+("Finance"),
+("Legal")
+;
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Shitkicker", 100000, 1),
+("Soulsucker", 80000, 1),
+("Crafty MoFo", 150000, 2),
+("Fubar-er", 40000, 2),
+("Ambulance Chaser", 200000, 4),
+("GoFer", 60000, 4),
+("Numbah Crunchah", 95000, 3),
+("Bit Byter", 76000, 3)
+;
+
+-- Managers
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES 
+("Jimmy", "Two Chins",1),
+("Jillian", "TakeNoShit", 3),
+("Martha", "TheHammah", 4)
+;
+
+-- Underlings
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+("Horatio", "GetErDone", 2, 3),
+("Julio", "GiveTwoShits", 5, 5),
+("Matty", "LadderClimbah", 8, 6),
+("JoJo", "EyesOnPrize", 7, 4)
+;
+
+
+SELECT id, first_name, last_name, title, name, salary, manager_id 
+FROM employee, role, department 
+WHERE employee.role_id = role.role_id AND role.department_id = department.department_id ORDER BY id;
+
+SELECT department.name, employee.first_name, employee.last_name, role.title 
+FROM ((role 
+INNER JOIN department ON department.department_id = role.department_id) 
+INNER JOIN employee ON role.role_id = employee.role_id) WHERE department.name = "Sales";
+
+SELECT first_name, last_name FROM employee WHERE manager_id IS NOT NULL;
+           
 
 
--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-// const util = require("util");
-// const fs = require("fs");
+const table = require("console.table");
+const figlet = require('figlet');
+
+figlet('Employee Manager', function (err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
 
 const connection = mysql.createConnection({
     host: "localhost",
+    user: "root",
     port: 3306,
     password: "Discodog@1",
     database: "employeeTracker_DB"
@@ -12,32 +22,34 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
     if (err) throw err;
+    console.log("DB connected!")
     start();
 });
 
 // questions
 
 function start() {
-    return inquirer
-        .prompt({
-            name: "action",
-            type: "list",
-            message: "What would you like to do?"
-        choices: ["View All Employees", "View All Employees By Department", "View All Employees By Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "View All Roles"]
-
-        })
+    inquirer
+        .prompt(
+            {
+                name: "action",
+                type: "list",
+                message: "What would you like to do?",
+                choices: ["View All Employees", "View All Employees By Department", "View All Employees By Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "View All Roles"]
+            }
+        )
         .then((answer) => {
             if (answer.action === "View All Employees") {
-                viewAllEmp()
+                viewAllEmp();
             }
             else if (answer.action === "View All Employees By Department") {
-                viewAllEmpDept()
+                viewAllEmpDept();
             }
             else if (answer.action === "View All Employees By Manager") {
-
+                viewAllEmpMan();
             }
             else if (answer.action === "Add Employee") {
-
+                addEmployee();
             }
             else if (answer.action === "Remove Employee") {
 
@@ -56,25 +68,86 @@ function start() {
 }
 
 function viewAllEmp() {
-    connection.query(
-        `SELECT id, first_name, last_name, title, department, salary, manager
-        FROM employee, role, department
-        WHERE employee.role_id = role.role_id
-        AND role.department_id = department.department_id
-        ORDER BY id;`,
+    connection.query("SELECT id, first_name, last_name, title, name, salary, manager_id FROM employee, role, department WHERE employee.role_id = role.role_id AND role.department_id = department.department_id ORDER BY id",
         function (err, results) {
             if (err) throw err;
-            console.log(results)
+            console.log("\n" + "\n")
+            console.table(results)
+            console.log("\n" + "Move arrows to make another choice")
         }
-    start();
     )
+    start()
 };
 
 function viewAllEmpDept() {
-
-
-    connection.query(
-        
-    )
-
+    inquirer
+        .prompt({
+            name: "dept",
+            type: "list",
+            message: "What department would you like to view employees for?",
+            choices: ["Engineering", "Finance", "Legal", "Sales"]
+        })
+        .then((answer) => {
+            var query = "SELECT department.name, employee.first_name, employee.last_name, role.title FROM ((role INNER JOIN department ON department.department_id = role.department_id) INNER JOIN employee ON role.role_id = employee.role_id) WHERE ?";
+            connection.query(query, { name: answer.dept }, function (err, res) {
+                if (err) throw err;
+                console.log("\n" + "\n")
+                console.table(res)
+                console.log("\n" + "Move arrows to make another choice")
+            })
+        })
+        .then(() => {
+            start();
+        })
 }
+
+function viewAllEmpMan() {
+
+    connection.query("SELECT first_name, last_name, id FROM employee WHERE manager_id IS NULL",
+        function (err, results) {
+            if (err) throw err;
+
+            inquirer
+                .prompt({
+                    name: "manager",
+                    type: "list",
+                    message: "What manager would you like to view employees for?",
+                    choices: function () {
+                        var choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(results[i].id + " " + results[i].first_name + " " + results[i].last_name);
+                        }
+                        return choiceArray;
+                    }
+                })
+                .then((answer) => {
+                    let query = "SELECT id, first_name, last_name, title, name, salary FROM employee LEFT JOIN role ON (employee.role_id = role.role_id) LEFT JOIN department ON (role.department_id = department.department_id) WHERE ?";
+
+                    let answerArray = Object.values(answer)
+                    let answerId = answerArray[0].split(" ")
+                    console.log(answerId[0])
+
+                    connection.query(query, { manager_id: answerId[0] }, function (err, res) {
+                        if (err) throw err;
+
+                        console.log(query)
+                        console.log("\n" + "\n")
+                        console.table(res)
+                        console.log("\n" + "Move arrows to make another choice")
+
+                    });
+                })
+                .then(() => {
+                    start();
+                })
+        });
+}
+
+// function addEmployee() {
+
+//     //do a bunch of these for each entry
+//     connection.query("INSERT INTO tasks (task) VALUES (?)", [req.body.task], function (err, result) {
+//         if (err) throw err;
+//         INSERT INTO tasks//table// (task//column) VALUES (?)
+//     }
+
